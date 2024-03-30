d5898097abe697c6d6d2a49140ea52666eee807f
Jashan Pratap Singh <jashanpratapsingh@jashans-air.wifi.int.ryerson.ca>
2024-03-30 15:48:58 -0400

    Making the first changes to the file and added a new one

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// Starting the code here, everyone should paste there codes here respectively and we will go through the codes together. I will be going through the codes from ques 1 to 
+
+//Question 1
+
+int main(void) {
+
+}
